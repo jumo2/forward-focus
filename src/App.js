e89f6310d50cbd886a814e870b78c75d303066e1
Judd Morgan <juddmorgan@gmail.com>
2021-02-02 04:40:15 +0000

    Rough outline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
+import logo from './assets/FF_NoBack.svg'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography'
 
 function App() {
   return (
-    <div >
-     <h1>Forward Focus</h1>
-     <Button variant="contained">Default</Button>
+      <div>
+          <AppBar position="static">
+  <Toolbar className="App">
+    <IconButton edge="start"  color="inherit" aria-label="menu">
+        <MenuIcon/>
+    </IconButton>
+  </Toolbar>
+</AppBar>
+    <Container maxWidth="xl" className="App"> 
+        <img src={logo}></img>
+    </Container>
     </div>
   );
 }
