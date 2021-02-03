e941a8fc65c56626c0a177b1cd3a720d8dabc1a2
Judd Morgan <juddmorgan@gmail.com>
2021-02-03 02:28:24 +0000

    Added additional logos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
-import logo from './assets/FF_NoBack.svg'
+import logo from './assets/FF_NoBack_FF_Only.svg'
+import appBarLogo from './assets/FF_NoBack_FullWord.svg'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid';
 
 function App() {
   return (
@@ -16,10 +18,21 @@ function App() {
     <IconButton edge="start"  color="inherit" aria-label="menu">
         <MenuIcon/>
     </IconButton>
+    <img src={appBarLogo}/>
   </Toolbar>
 </AppBar>
-    <Container maxWidth="xl" className="App"> 
-        <img className="Logo" src={logo}></img>
+    <Container maxWidth="xl" className="App">
+        <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+               
+            </Grid>
+            <Grid 
+            justify="flex-start"
+            alignItems="center"
+            item xs={12} sm={6}>
+                <img className="Logo" src={logo}></img>
+            </Grid>
+        </Grid>
     </Container>
     </div>
   );
