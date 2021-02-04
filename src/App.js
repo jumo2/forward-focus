82fb56d55d17c7eaa98dc427e440e25be8b84db1
Judd Morgan <juddmorgan@gmail.com>
2021-02-04 03:26:47 +0000

    Added Services Section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
-import logo from './assets/Ff_Everywhere.svg'
+import logo from './assets/FF_NoBack_FF_Only.svg'
 import appBarLogo from './assets/FF_NoBack_FullWord.svg'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
@@ -25,27 +25,62 @@ function App() {
                     <img src={appBarLogo} />
                 </Toolbar>
             </AppBar>
-            <Container maxWidth="xl" className="App">
+            <Container maxWidth="md" className="App">
                 <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
+                    <Grid justify="flex-end" alignItems="flex-end"
+                     item xs={12} sm={12}>
                         <Card className="card-dark" elevation={0}>
                             <CardContent>
-                                <h1>Helping individuals achieve their goals...</h1>
-                                <p>We can help you by developing a customize plan that will <em>focus</em> on moving your <em>forward</em> in your journey</p>
+                                <h1>Helping individuals put their future in <em style={{color: "red"}}>focus</em>.</h1>
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p> 
                             </CardContent>
                             <CardActions className="card-action-custom">
                                 <Button className="join-now-button" variant="outlined" color="red">Join Now</Button>
                             </CardActions>
                         </Card>
                     </Grid>
-                    <Grid
+                    {/* <Grid
                         direction="row"
-                        justify="flex-start"
-                        alignItems="flex-start"
+                        justify="flex-end"
+                        alignItems="flex-end"
                         item xs={12} sm={6}>
                         <img className="Logo" src={logo}></img>
-                    </Grid>
+                    </Grid>*/}
                 </Grid>
+                <br></br> 
+            </Container>
+
+            {/* Services Section */}
+            <Container style={{background: "white"}}>
+                <Grid container spacing={2}>
+                    <Grid justify="flex-end" alignItems="flex-end"
+                     item xs={12} sm={4}>
+                         <Card  elevation={3}>
+                            <CardContent>
+                                <h2>Leadership Coaching</h2>
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                            </CardContent>
+                         </Card>
+                     </Grid>
+                                         <Grid justify="flex-end" alignItems="flex-end"
+                     item xs={12} sm={4}>
+                         <Card  elevation={3}>
+                            <CardContent>
+                               <h2>Youth Mentorship</h2>
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                            </CardContent>
+                         </Card>
+                     </Grid>
+                                         <Grid justify="flex-end" alignItems="flex-end"
+                     item xs={12} sm={4}>
+                         <Card  elevation={3}>
+                            <CardContent>
+                                <h2>Adult Mentorship</h2>
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                            </CardContent>
+                         </Card>
+                     </Grid>
+                </Grid>               
             </Container>
         </div>
     );
