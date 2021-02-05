b47fd4edd639d82e1ef875040bf85ca52530ef95
Judd Morgan <juddmorgan@gmail.com>
2021-02-05 02:50:28 +0000

    Updated Services Section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import logo from './assets/FF_NoBack_FF_Only.svg'
 import appBarLogo from './assets/FF_NoBack_FullWord.svg'
-import blob from './assets/blob.svg'
+import analysis from './assets/analysis.svg'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
@@ -13,6 +13,7 @@ import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import { Paper } from '@material-ui/core';
 
 
 function App() {
@@ -26,14 +27,14 @@ function App() {
                     <img src={appBarLogo} />
                 </Toolbar>
             </AppBar>
-            <Container maxWidth="md" className="App">
+            <Container className="section" maxWidth="md" className="App">
                 <Grid container spacing={2}>
                     <Grid justify="flex-end" alignItems="flex-end"
-                     item xs={12} sm={12}>
+                        item xs={12} sm={12}>
                         <Card className="card-dark" elevation={0}>
                             <CardContent>
-                                <h1>Helping individuals put their future in <em style={{color: "red"}}>focus</em>.</h1>
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p> 
+                                <h1>Helping individuals put their future in <em style={{ color: "red" }}>focus</em>.</h1>
+                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
                             </CardContent>
                             <CardActions className="card-action-custom">
                                 <Button className="join-now-button" variant="outlined" color="red">Join Now</Button>
@@ -48,17 +49,42 @@ function App() {
                         <img className="Logo" src={logo}></img>
                     </Grid>*/}
                 </Grid>
-                
+
             </Container>
-            <div  className="wave-container">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#282c34" fill-opacity="1" d="M0,224L60,229.3C120,235,240,245,360,250.7C480,256,600,256,720,234.7C840,213,960,171,1080,138.7C1200,107,1320,85,1380,74.7L1440,64L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
+            <div className="wave-container">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#282c34" fill-opacity="1" d="M0,224L60,229.3C120,235,240,245,360,250.7C480,256,600,256,720,234.7C840,213,960,171,1080,138.7C1200,107,1320,85,1380,74.7L1440,64L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
             </div>
 
 
             {/* Services Section */}
-            <Container style={{background: "white"}}>
-                
-                <Grid container spacing={2}>
+            <Container className="section" style={{ background: "white" }}>
+                <Grid
+                    container
+                    spacing={0}
+                    direction="row"
+                    justify="space-evenly"
+                    alignItems="center"
+                >
+                    <Grid justify="center" alignItems="center"
+                        item xs={12} sm={6}>
+                        <img className="analysis-svg" src={analysis}></img>
+                    </Grid>
+                    <Grid direction="column" justify="flex-end" alignItems="flex-end"
+                        item xs={12} sm={6}>
+                        <Paper elevation="0">
+                            <h2>Leadership Coaching</h2>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                        </Paper>
+                        <Paper elevation="0">
+                            <h2>Personal Mentorship</h2>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                        </Paper>
+                    </Grid>
+                </Grid>
+
+
+
+                {/* <Grid container spacing={2}>
                     <Grid justify="flex-end" alignItems="flex-end"
                      item xs={12} sm={4}>
                          <Card  elevation={3}>
@@ -68,7 +94,7 @@ function App() {
                             </CardContent>
                          </Card>
                      </Grid>
-                                         <Grid justify="flex-end" alignItems="flex-end"
+                    <Grid justify="flex-end" alignItems="flex-end"
                      item xs={12} sm={4}>
                          <Card  elevation={3}>
                             <CardContent>
@@ -89,12 +115,35 @@ function App() {
                 </Grid>
                 <Grid>
                     
-                </Grid>               
+                </Grid> */}
+                </Container>
+
+{/* About Section */}
+
+<Container className="section" style={{background: "white"}}>
+<Grid
+                    container
+                    spacing={0}
+                    direction="row"
+                    justify="space-evenly"
+                    alignItems="flex-end"
+                >
+
+                    <Grid  direction="column" justify="flex-end" alignItems="flex-end"
+                        item xs={12} sm={6}>
+                        
+                    </Grid>
+                                        <Grid justify="center" alignItems="center"
+                        item xs={12} sm={6}>
+                        
+                    </Grid>
+                </Grid>
             </Container>
 
-        <div className="wave-footer-container">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#273036" fill-opacity="1" d="M0,224L60,229.3C120,235,240,245,360,250.7C480,256,600,256,720,234.7C840,213,960,171,1080,138.7C1200,107,1320,85,1380,74.7L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
-        </div>
+
+            <div className="wave-footer-container">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#273036" fill-opacity="1" d="M0,224L60,229.3C120,235,240,245,360,250.7C480,256,600,256,720,234.7C840,213,960,171,1080,138.7C1200,107,1320,85,1380,74.7L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+            </div>
 
         </div>
     );
